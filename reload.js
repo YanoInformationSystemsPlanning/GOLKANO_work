2882e312f1af76a260c94ebfd224c0a3fbe61495
Akira Yano <yano.system.planning@gmail.com>
2025-07-24 13:23:45 +0900

    Update reload.js

--- reload.js
+++ reload.js
@@ -1,8 +1,8 @@
 // reload.js
-// iOS Safari対策: キャッシュ復帰時に index.html へリダイレクト
+// iOS Safari対策: キャッシュ復帰時に index.html をキャッシュバスター付きで再読み込み
 window.addEventListener('pageshow', function(event) {
   if (event.persisted) {
-    // キャッシュから復帰した場合、強制的に index.html へ遷移
-    window.location.href = "index.html";
+    // キャッシュから復帰した場合、強制的にバージョンパラメータ付きでリロード
+    window.location.href = "index.html?v=" + Date.now();
   }
 });
